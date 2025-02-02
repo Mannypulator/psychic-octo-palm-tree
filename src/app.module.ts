3f11f6c71c96327dcd771e8db760e36ef0e9f213
Mannypulator <emeritus93@gmail.com>
2025-02-03 00:08:51 +0100

    updated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ClassifyNumberController } from './classify-number/classify-number.controller';
 import { ClassifyNumberService } from './classify-number/classify-number.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HttpModule],
   controllers: [ClassifyNumberController],
   providers: [ClassifyNumberService],
 })
--- src/classify-number/classify-number.service.ts
+++ src/classify-number/classify-number.service.ts
@@ -1,11 +1,47 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ClassifyNumberService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
 
+  async classifyNumber(num: number) {
+    const prime = this.isPrime(num);
+    const perfect = this.isPerfect(num);
+    const armstrong = this.isArmstrong(num);
+    const sumDigits = this.digitSum(num);
+    const parity = num % 2 === 0 ? 'even' : 'odd';
+
+    // Determine the properties array
+    const properties: string[] = [];
+    if (armstrong) properties.push('armstrong');
+    properties.push(parity);
+
+    // Get a fun fact from the Numbers API
+    const baseUrl = this.configService.get<string>('NUMBER_API_BASE_URL');
+    const url = `${baseUrl}/${num}/math`;
+    let fun_fact = '';
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      fun_fact = response.data;
+    } catch (error) {
+      fun_fact = 'Fun fact not available.';
+    }
+
+    return {
+      number: num,
+      is_prime: prime,
+      is_perfect: perfect,
+      properties,
+      digit_sum: sumDigits,
+      fun_fact,
+    };
+  }
   // Check if a number is prime
   private isPrime(num: number): boolean {
     if (num <= 1) return false;
@@ -44,37 +80,5 @@ export class ClassifyNumberService {
       .toString()
       .split('')
       .reduce((acc, digit) => acc + Number(digit), 0);
-  }
-
-  async classifyNumber(num: number) {
-    const prime = this.isPrime(num);
-    const perfect = this.isPerfect(num);
-    const armstrong = this.isArmstrong(num);
-    const sumDigits = this.digitSum(num);
-    const parity = num % 2 === 0 ? 'even' : 'odd';
-
-    // Determine the properties array
-    const properties: string[] = [];
-    if (armstrong) properties.push('armstrong');
-    properties.push(parity);
-
-    // Get a fun fact from the Numbers API
-    let fun_fact = '';
-    try {
-      const url = `http://numbersapi.com/${num}/math`;
-      const response = await firstValueFrom(this.httpService.get(url));
-      fun_fact = response.data;
-    } catch (error) {
-      fun_fact = 'Fun fact not available.';
-    }
-
-    return {
-      number: num,
-      is_prime: prime,
-      is_perfect: perfect,
-      properties,
-      digit_sum: sumDigits,
-      fun_fact,
-    };
   }
 }
